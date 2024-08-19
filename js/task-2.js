86a39829b7e3b28ff9bf4937a15c96298a6fdb49
YRoman Turas <yosalden.com@gmail.com>
2024-08-19 14:20:59 +0300

    Add homework tasks

---
+++ js/task-2.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/**
+ * Getting users with friends
+ *
+ * @param {Array} array of objects users
+ * @param {String} friendName
+ *
+ * @return {Array} array of objects users
+ */
+function getUsersWithFriend(users, friendName) {
+    return users.filter(user => user.friends.includes(friendName));
+}
+
+// -----------< Tests >-----------
+const allUsers = [
+    {
+        name: 'Moore Hensley',
+        friends: ['Sharron Pace'],
+    },
+    {
+        name: 'Sharlene Bush',
+        friends: ['Briana Decker', 'Sharron Pace'],
+    },
+    {
+        name: 'Ross Vazquez',
+        friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    },
+    {
+        name: 'Elma Head',
+        friends: ['Goldie Gentry', 'Aisha Tran'],
+    },
+    {
+        name: 'Carey Barr',
+        friends: ['Jordan Sampson', 'Eddie Strong'],
+    },
+    {
+        name: 'Blackburn Dotson',
+        friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    },
+    {
+        name: 'Sheree Anthony',
+        friends: ['Goldie Gentry', 'Briana Decker'],
+    },
+];
+
+console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // []
